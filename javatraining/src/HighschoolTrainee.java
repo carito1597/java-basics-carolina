9a3d466253995a03bf1ae83978eadd2932357ab9
Carolina Villalobos <carito15villalobos@gmail.com>
2022-11-11 12:11:01 -0400

    modify last name

--- javatraining/src/HighschoolTrainee.java
+++ javatraining/src/HighschoolTrainee.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 public class HighschoolTrainee {
-        public void main() {
+        public static void main(String[] args) {
             int age = 25;
             double prom = 15.0;
-            char firstInitial = 'c';
-            char lastInitial = 'v';
+
+            String firstName = "Carolina";
+            String lastName = "Villalobos";
+            char firstInitial = firstName.charAt(0);
+            char lastInitial = lastName.charAt(0);
             boolean attendance = true;
 
+            String studentName = firstName+" "+lastName;
+
+            System.out.println("The student name is " + studentName);
+            System.out.println("The first name is " + firstName);
+            System.out.println("The last name is " + lastName);
             System.out.println("The student age is " + age);
             System.out.println("The prom is " + prom);
             System.out.println("The first initial is " + firstInitial);
             System.out.println("The last initial is " + lastInitial);
             System.out.println("The attendance is " + attendance);
+
+            System.out.println("The student prom are " + prom + ". Please modify this prom");
+
+            Scanner input = new Scanner(System.in);
+            prom = input.nextDouble();
+
+            System.out.println("The new prom are " + prom);
+
+            System.out.println("The last name is " + lastName + ". Please modify this prom: ");
+            lastName = input.nextLine();
+            System.out.println("The new last name is " + lastName);
         }
 }
--- javatraining/src/Main.java
+++ javatraining/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello I am Trainee");
-        HighschoolTrainee trainee = new HighschoolTrainee();
-        trainee.main();
     }
 }
