6cff382fb1e3fefb50002fb8c8612e44f336ca10
Carolina Villalobos <carito15villalobos@gmail.com>
2022-11-11 11:37:53 -0400

    personal information highclassTrainee

---
+++ javatraining/src/HighschoolTrainee.java
@@ -0,0 +1,15 @@
+public class HighschoolTrainee {
+        public void main() {
+            int age = 25;
+            double prom = 15.0;
+            char firstInitial = 'c';
+            char lastInitial = 'v';
+            boolean attendance = true;
+
+            System.out.println("The student age is " + age);
+            System.out.println("The prom is " + prom);
+            System.out.println("The first initial is " + firstInitial);
+            System.out.println("The last initial is " + lastInitial);
+            System.out.println("The attendance is " + attendance);
+        }
+}
